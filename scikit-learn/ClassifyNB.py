23110b9a122182cb80f0b0bb8c7d570a470bcfad
swaption2009 <swaption2009@gmail.com>
2016-11-05 13:19:35 -0700

    import GaussianNB from sklearnNB and generate chart

--- scikit-learn/ClassifyNB.py
+++ scikit-learn/ClassifyNB.py
@@ -1,10 +1,15 @@
 def classify(features_train, labels_train):
 
-### import the sklearn module for GaussianNB
-### create classifier
-### fit the classifier on the training features and labels
-### return the fit classifier
+    ### import the sklearn module for GaussianNB
+    from sklearn.naive_bayes import GaussianNB
 
+    ### create classifier
+    clf = GaussianNB()
 
-### your code goes here!
+    ### fit the classifier on the training features and labels
+    clf.fit(features_train, labels_train)
 
+    ### return the fit classifier
+    return clf
+
+    ### your code goes here!
