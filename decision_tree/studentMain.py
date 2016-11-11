fd631b4edbc7258ee354839941418d4f4d99f8d9
swaption2009 <swaption2009@gmail.com>
2016-11-11 13:16:38 -0800

    decision tree accuracy with min_samples_split

--- decision_tree/studentMain.py
+++ decision_tree/studentMain.py
@@ -35,3 +35,25 @@ print "accuracy score: ", acc
 def submitAccuracies():
     return {"acc": round(acc, 3)}
 
+
+
+### Decision tree accuracy with min_samples_split 2 and 50
+from sklearn import tree
+
+clf2 = tree.DecisionTreeClassifier(min_samples_split = 2)
+clf2 = clf2.fit(features_train, labels_train)
+pred2 = clf2.predict(features_test)
+acc_min_samples_split_2 = accuracy_score(pred2, labels_test)
+
+clf50 = tree.DecisionTreeClassifier(min_samples_split = 50)
+clf50 = clf50.fit(features_train, labels_train)
+pred50 = clf50.predict(features_test)
+acc_min_samples_split_50 = accuracy_score(pred50, labels_test)
+
+print "Accuracy with min_samples_split = 2 is", acc_min_samples_split_2
+print "Accuracy with min_samples_split = 50 is", acc_min_samples_split_50
+
+def submitAccuracies():
+  return {"acc_min_samples_split_2":round(acc_min_samples_split_2,3),
+          "acc_min_samples_split_50":round(acc_min_samples_split_50,3)}
+
